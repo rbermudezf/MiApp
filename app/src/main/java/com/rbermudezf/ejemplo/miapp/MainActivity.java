ae981881181d27561d2798712ef3d65a12ddcc1c
Ronald Bermudez <rbermudezf@hotmail.com>
2018-10-30 10:02:16 -0500

    Subiendo cambios 2

--- app/src/main/java/com/rbermudezf/ejemplo/miapp/MainActivity.java
+++ app/src/main/java/com/rbermudezf/ejemplo/miapp/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Comprobando que funciona control de versiones", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Comprobando control de versiones", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
